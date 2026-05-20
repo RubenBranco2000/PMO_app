1d91d6408915f6f2643c040484f51571fda47b34
Rúben Branco <ruben.branco@innowave.tech>
2026-05-20 17:25:14 +0100

    Added login validation test

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,7 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
 test('homepage loads', async ({ page }) => {
-  await page.goto('/');
 
-  await expect(page).not.toHaveTitle('');
+  await page.goto('https://devfrontendstorage.z6.web.core.windows.net/');
+
+  await expect(page).toHaveURL(/.*login.*/);
+
+  await expect(page.locator('text=Sign in')).toBeVisible();
+
 });
